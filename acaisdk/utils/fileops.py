82ca162498543278d42710da9dc002a5d141832e
changx <xuchang0310@gmail.com>
2019-11-19 20:58:10 -0500

    Change file upload to platform-agnostic

--- acaisdk/utils/fileops.py
+++ acaisdk/utils/fileops.py
@@ -1,5 +1,7 @@
 import os
 import signal
+import multiprocessing
+
 from tqdm import tqdm
 from acaisdk.utils.rest_utils import get_session
 from acaisdk.utils import utils
@@ -15,7 +17,8 @@ class FileIO:
     def get_file_size(path):
         return os.path.getsize(path)
 
-    def upload(self, presigned_link: str):
+    def __upload_old(self, presigned_link: str):
+        """Deprecated, use upload() instead. This is not portable."""
         file_object = open(self.file_path, 'rb')
         pid = os.fork()
         if pid == 0:
@@ -46,6 +49,40 @@ class FileIO:
         finally:
             file_object.close()
             os.kill(pid, signal.SIGKILL)
+
+    def upload(self, presigned_link: str):
+        def progress_bar(file_obj, file_size):
+            prev_pos = 0
+            with tqdm(total=file_size,
+                      unit='B', unit_scale=True, unit_divisor=1024,
+                      ascii=True, disable=not utils.IS_CLI) as p_bar:
+                while 1:
+                    offset = file_obj.tell()
+                    p_bar.update((offset - prev_pos))
+                    prev_pos = offset
+                    if offset >= file_size:
+                        break
+
+        file_object =  open(self.file_path, 'rb')
+
+        p = multiprocessing.Process(target=progress_bar, args=(file_object,
+                                                               self.file_size))
+        p.start()
+
+        headers = {'Content-Type': 'application/binary'}
+        try:
+            r = get_session().put(presigned_link, data=file_object,
+                                  headers=headers)
+            if file_object.tell() == 0:
+                # Hack, somehow S3 presigned url upload does not recognize
+                # fd for an empty file but can upload with data=None.
+                r = get_session().put(presigned_link, data=None,
+                                      headers=headers)
+            return r
+        finally:
+            file_object.close()
+            p.kill()
+
 
     @staticmethod
     def download(presigned_link: str,
