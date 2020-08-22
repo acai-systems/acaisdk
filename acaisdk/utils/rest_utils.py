3f7f5f63af91c4ec8b0a16c38352c0f769122163
Baljit Singh <baljit92@gmail.com>
2020-08-22 10:19:36 -0500

    reverted https to http for temporary fix

--- acaisdk/utils/rest_utils.py
+++ acaisdk/utils/rest_utils.py
@@ -17,7 +17,7 @@ def get_session() -> requests.Session:
 
 def post(server, port, service, path,
          params: dict, data: dict):
-    r = get_session().post('https://{}:{}/{}/{}'
+    r = get_session().post('http://{}:{}/{}/{}'
                            .format(server, port, service, path),
                            params=params,
                            json=data,
@@ -28,7 +28,7 @@ def post(server, port, service, path,
 
 
 def get(server, port, service, path, params: dict):
-    r = get_session().get('https://{}:{}/{}/{}'
+    r = get_session().get('http://{}:{}/{}/{}'
                           .format(server, port, service, path),
                           params=params, verify=False)
     if r.status_code != HTTPStatus.OK:
