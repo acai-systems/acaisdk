b4f2079d59098a3f08b79bf6c750f42962588d19
Jin Han <jinhan2019@u.northwestern.edu>
2020-10-19 01:45:30 -0400

    Revert "Revert "update sdk to calculate fileset hash""

--- acaisdk/fileset.py
+++ acaisdk/fileset.py
@@ -1,6 +1,7 @@
 from acaisdk.services.api_calls import *
 from typing import Iterable, List
 from acaisdk import file
+from acaisdk import meta
 from acaisdk.utils.fileops import FileIO
 
 
@@ -75,9 +76,14 @@ class FileSet:
             _msg = 'Need a list or tuple of remote entities ' \
                    'instead of {}.'.format(type(remote_entities))
             raise AcaiException(_msg)
+        
+        files_hash_strings = [meta.Meta.get_file_meta(r)['data'][0]['__hash__'] for r in sorted(remote_entities)]
+        fileset_hash = md5_string_list(files_hash_strings)
+
         data = {
             "name": file_set_name,
-            "files": list(remote_entities)
+            "files": list(remote_entities),
+            "hash": fileset_hash
         }
         return RestRequest(StorageApi.create_file_set) \
             .with_data(data) \
