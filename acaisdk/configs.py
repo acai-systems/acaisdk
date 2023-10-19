8f91769aa907d7d2321f5995fc38bf4c591ec8b4
Nishant Ravi Shankar <nishantravishankar@Nishants-MacBook-Pro-3.local>
2023-10-19 18:15:18 -0400

    config change

--- acaisdk/configs.py
+++ acaisdk/configs.py
@@ -22,7 +22,7 @@ class Configs:
         :return: None
         """
         if not config_path:
-            config_path = os.path.join(os.path.dirname(__file__),
+            config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                        CONFIG_FILENAME)
 
         conf = configparser.ConfigParser()
